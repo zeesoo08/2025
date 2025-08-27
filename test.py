07d87eb5544f7d204f6e07e06fc47222e881507f
zeesoo08 <mjt0103@naver.com>
2025-08-27 20:28:26 +0900

    Update test.py

--- test.py
+++ test.py
@@ -16,19 +16,8 @@ brushing = st.radio(
 if st.button("확인하기 ✅"):
     if brushing == 1:
         st.warning("🦷 노력해! 내일은 조금 더 해보자 💪")
-        st.image(
-          " https://everysmile.co.za/blog/what-are-the-signs-of-early-tooth-decay/이미지파일이름.jpg"
-
-        "⚠️ 양치를 게을리하면 이렇게 될 수 있어요!",
-            use_column_width=True
-        )
     elif brushing == 2:
         st.info("😃 잘하고 있어! 조금만 더 하려고 노력하자 ✨")
     elif brushing == 3:
         st.success("👏 정말 잘했어! 입안이 개운하겠는걸?! 🌟🪥")
         st.balloons()  # 🎉 3번일 때만 폭죽 효과
-        st.image(
-            "https://images.pexels.com/photos/52527/teeth-smile-dental-health-mouth-52527.jpeg",  
-            caption="✨ 건강하고 깨끗한 치아예요!",
-            use_column_width=True
-        )
