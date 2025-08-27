8d155b93083f1ad4892b8f2089e5716275384498
zeesoo08 <mjt0103@naver.com>
2025-08-27 20:10:46 +0900

    Update test.py

--- test.py
+++ test.py
@@ -16,11 +16,8 @@ brushing = st.radio(
 if st.button("확인하기 ✅"):
     if brushing == 1:
         st.warning("🦷 노력해! 내일은 조금 더 해보자 💪")
-        st.image("bad_teeth.jpg", caption="⚠️ 양치를 게을리하면 이렇게 돼요!", use_column_width=True)
     elif brushing == 2:
         st.info("😃 잘하고 있어! 조금만 더 하려고 노력하자 ✨")
-        st.image("cavity_teeth.jpg", caption="❗ 충치가 생길 수 있어요!", use_column_width=True)
     elif brushing == 3:
         st.success("👏 정말 잘했어! 입안이 개운하겠는걸?! 🌟🪥")
-        st.image("clean_teeth.jpg", caption="✨ 건강하고 깨끗한 치아!", use_column_width=True)
-        st.balloons()
+        st.balloons()  # 🎉 3번일 때만 폭죽 효과
